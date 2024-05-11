59992993aa5b3fa338a33a9615514ec79d83141c
shriyashshukla <harishukla797@gmail.com>
2024-05-12 00:59:54 +0530

    ...

--- src/app/api/send/route.js
+++ src/app/api/send/route.js
@@ -1,13 +1,14 @@
+import { NextResponse } from "next/server";
 import { Resend } from "resend";
 
-const resendClient = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend(process.env.RESEND_API_KEY);
 const fromEmail = process.env.FROM_EMAIL;
 
 export async function POST(req, res) {
   const { email, subject, message } = await req.json();
   console.log(email, subject, message);
   try {
-    const data = await resendClient.emails.send({
+    const data = await resend.emails.send({
       from: fromEmail,
       to: [fromEmail, email],
       subject: subject,
